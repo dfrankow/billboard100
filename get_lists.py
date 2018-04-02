c40a227495f540fd74dfa1a8a04daade2803a77d
Dan F <dfrankow@gmail.com>
2018-04-02 08:20:30 -0500

    flakes

--- get_lists.py
+++ get_lists.py
@@ -6,7 +6,7 @@ import pywikibot
 
 site = pywikibot.Site('en', 'wikipedia')  # The site we want to run our bot on
 
-for year in xrange(1958,2019):
+for year in xrange(1958, 2019):
     pagename = 'List_of_Billboard_Hot_100_number-one_singles_of_%d' % year
     print pagename
     filename = 'pages/%s.wiki' % pagename
--- parse_lists.py
+++ parse_lists.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import os
-import re
 
 import wikitextparser as wtp
 
